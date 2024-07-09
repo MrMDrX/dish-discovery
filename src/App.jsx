0c6721e7235484ccbba1b04fd872fdfae744c4ce
MrMDrX <geek.4tech007@gmail.com>
2024-07-09 09:25:17 +0000

    Add DaisyUI 💄

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-blue-600">Dish Dicovery</h1>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Dish Discovery</h1>
+            <p className="py-6">Discover Your Next Favorite Dish! Welcome to Dish Discovery, where culinary adventure awaits! Explore a world of diverse recipes tailored to every taste and occasion. Dive in, get inspired, and start cooking today!</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
