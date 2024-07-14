92afd8076a49f59e8e964e6161abd89c80082a1f
MrMDrX <geek.4tech007@gmail.com>
2024-07-14 00:30:17 +0000

    Minor fixes 🩹

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -11,6 +11,7 @@ const FavoritesPage = () => {
         {favorites.length === 0 && (
           <div className="h-[80vh] flex flex-col items-center gap-4">
             <img src="/404.svg" className="h-3/4" alt="404 svg" />
+            <p className="text-center text-lg font-semibold">Opps! No favorites yet.</p>
           </div>
         )}
 
